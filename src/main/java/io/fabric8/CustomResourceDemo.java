1e2f725f88a092b8b7cbb0e753c41dd6e0c612bd
Rohan Kumar <rohaan@redhat.com>
2019-04-18 14:13:29 +0530

    Added Custom Resource Demo and CustomResourceSchemaValidation

--- src/main/java/io/fabric8/CustomResourceDemo.java
+++ src/main/java/io/fabric8/CustomResourceDemo.java
@@ -3,27 +3,20 @@ package io.fabric8;
 import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 public class CustomResourceDemo {
     private static final Logger logger = Logger.getLogger(CustomResourceDemo.class.getName());
 
     public static void main(String args[]) throws IOException {
-        String customResourceDefinitionPath = "/home/Rohaan/go/src/github.com/rohanKanojia/hello-operator/deploy/crds/test_v1alpha1_hello_crd.yaml";
-        String customResourcePath = "/home/Rohaan/go/src/github.com/rohanKanojia/hello-operator/deploy/crds/test_v1alpha1_hello_cr.yaml";
-
 
         try (final KubernetesClient client = new DefaultKubernetesClient()) {
-            CustomResourceDefinition crd = client.customResourceDefinitions().load(customResourceDefinitionPath).get();
+            CustomResourceDefinition crd = client.customResourceDefinitions().load(CustomResourceDemo.class.getResourceAsStream("/hello-crd.yml")).get();
             client.customResourceDefinitions().create(crd);
+            log("Created Custom Resource Definition");
 
             CustomResourceDefinitionContext myCrdContext = new CustomResourceDefinitionContext.Builder()
                     .withGroup("test.fabric8.io")
@@ -32,7 +25,8 @@ public class CustomResourceDemo {
                     .withPlural("hellos")
                     .build();
 
-            client.customResource(myCrdContext).create("default", new FileInputStream(customResourcePath));
+            client.customResource(myCrdContext).create("default", CustomResourceDemo.class.getResourceAsStream("/hello-cr.yml"));
+            log("Successfully created Custom Resource");
         }
     }
 
---
+++ src/main/java/io/fabric8/CustomResourceSchemaValidation.java
@@ -0,0 +1,96 @@
+package io.fabric8;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition;
+import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinitionBuilder;
+import io.fabric8.kubernetes.api.model.apiextensions.JSONSchemaProps;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+public class CustomResourceSchemaValidation {
+    private static final Logger logger = Logger.getLogger(CustomResourceSchemaValidation.class.getName());
+
+    public static void main(String args[]) throws IOException {
+
+
+        try (final KubernetesClient client = new DefaultKubernetesClient()) {
+            JSONSchemaProps schema = readSchema();
+
+            CustomResourceDefinitionBuilder builder = new CustomResourceDefinitionBuilder()
+                    .withApiVersion("apiextensions.k8s.io/v1beta1")
+                    .withNewMetadata().withName("crontabs.stable.example.com").endMetadata()
+                    .withNewSpec()
+                    .withGroup("stable.example.com")
+                    .withVersion("v1")
+                    .withScope("Namespaced")
+                    .withNewNames()
+                    .withPlural("crontabs")
+                    .withSingular("crontab")
+                    .withKind("CronTab")
+                    .addToShortNames("ct")
+                    .endNames()
+                    .withNewValidation()
+                    .withNewOpenAPIV3SchemaLike(schema)
+                    .endOpenAPIV3Schema()
+                    .endValidation()
+                    .endSpec();
+
+            CustomResourceDefinition crd = builder.build();
+            crd.getSpec().getValidation().getOpenAPIV3Schema().setDependencies(null);
+            new DefaultKubernetesClient().customResourceDefinitions().create(builder.build());
+            System.out.println(schema.toString());
+//            CustomResourceDefinitionBuilder builder = new CustomResourceDefinitionBuilder()
+//                    .withApiVersion("apiextensions.k8s.io/v1beta1")
+//                    .withNewMetadata().withName("sparkclusters.radanalytics.io")
+//                    .endMetadata()
+//                    .withNewSpec()
+//                    .withNewNames()
+//                    .withKind("SparkCluster")
+//                    .withPlural("sparkclusters")
+//                    .endNames()
+//                    .withGroup("radanalytics.io")
+//                    .withVersion("v1")
+//                    .withScope("Namespaced")
+//                    .withNewValidation()
+//                    .withNewOpenAPIV3SchemaLike(schema)
+//                    .endOpenAPIV3Schema()
+//                    .endValidation()
+//                    .endSpec();
+//
+//            new DefaultKubernetesClient().customResourceDefinitions().createOrReplace(builder.build());
+
+//            KubernetesClient client2 = new DefaultKubernetesClient();
+//            CustomResourceDefinition crd2 = client.customResourceDefinitions().load(CustomResourceSchemaValidation.class.getResourceAsStream("/my-custom-resource-file.yml")).get();
+//            client.customResourceDefinitions().create(crd2);
+
+
+
+        }
+    }
+
+
+    public static JSONSchemaProps readSchema() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        URL in = CustomResourceSchemaValidation.class.getResource("/simple-schema.json");
+//        URL in = CustomResourceSchemaValidation.class.getResource("/sparkCluster.json");
+        if (null == in) {
+            return null;
+        }
+        try {
+            return mapper.readValue(in, JSONSchemaProps.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void log(String action) {
+        logger.info(action);
+    }
+}
