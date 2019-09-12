642a3e9fcfe8cb49fea43d1596e2ce88ebe655df
Rohan Kumar <rohaan@redhat.com>
2019-09-12 18:45:21 +0530

    Updated kubernetes-client to 4.5.1

---
+++ src/main/java/io/fabric8/CascadeDeletionDemo.java
@@ -0,0 +1,30 @@
+package io.fabric8;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CascadeDeletionDemo {
+    private static final Logger logger = Logger.getLogger(CascadeDeletionDemo.class
+            .getName());
+
+    public static void main(String args[]) {
+
+        try {
+            logger.log(Level.INFO, "Creating Kubernetes client");
+            KubernetesClient client = new DefaultKubernetesClient();
+            logger.log(Level.INFO, "Kubernetes client successfully created");
+
+            // Run command kubectl run nginx --image=nginx --replicas=1
+            client.apps().deployments().inNamespace("default").withName("nginx").delete();
+
+            client.close();
+        } catch (KubernetesClientException aException) {
+            logger.log(Level.SEVERE, "Problem encountered in Kubernetes Client");
+            aException.printStackTrace();
+        }
+    }
+}
