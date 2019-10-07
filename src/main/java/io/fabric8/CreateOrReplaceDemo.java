10226a5655eb5b781784827a4faa5879e53d6f7b
Rohan Kumar <rohaan@redhat.com>
2019-10-07 18:42:25 +0530

    Added Custom resource watch demo

---
+++ src/main/java/io/fabric8/CreateOrReplaceDemo.java
@@ -0,0 +1,62 @@
+package io.fabric8;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.api.model.IntOrString;
+import io.fabric8.kubernetes.api.model.KubernetesList;
+import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
+public class CreateOrReplaceDemo {
+    private static Logger logger = LoggerFactory.getLogger(CreateOrReplaceDemo.class);
+
+    public static void main(String args[]) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            String namespace = "default";
+
+            Service service =  new ServiceBuilder()
+                    .withNewMetadata().withName("my-service").endMetadata()
+                    .withNewSpec()
+                    .addToSelector("app", "Myapp")
+                    .addNewPort().withProtocol("TCP").withPort(80).withTargetPort(new IntOrString(9376)).endPort()
+                    .endSpec()
+                    .build();
+
+
+            ConfigMap configMap = new ConfigMapBuilder()
+                    .withNewMetadata().withName("my-configmap").endMetadata()
+                    .addToData(Collections.singletonMap("app", "Myapp"))
+                    .build();
+
+            logger.info("1. Creating list...");
+            KubernetesList list = new KubernetesListBuilder()
+                    .withItems(client.apps().deployments().load(CreateOrReplaceDemo.class.getResourceAsStream("/nginx-deployment.yml")).get()
+                    , service, configMap)
+                    .build();
+
+            logger.info("2. Applying list onto cluster...");
+            client.resourceList(list).inNamespace(namespace).createOrReplace();
+
+            logger.info("3. Modifying service to use new port ...");
+            service.getSpec().getPorts().get(0).setTargetPort(new IntOrString(9999));
+            list.getItems().remove(0);
+            list.getItems().add(service);
+
+            logger.info("4. Applying updated list...");
+            client.resourceList(list).inNamespace(namespace).createOrReplace();
+
+            logger.info("5. Deleting existing resources...");
+            client.resourceList(list).inNamespace(namespace).deletingExisting();
+        }
+    }
+}
---
+++ src/main/java/io/fabric8/WatchCustomResourceDemo.java
@@ -0,0 +1,62 @@
+package io.fabric8;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class WatchCustomResourceDemo {
+    private static Logger logger = LoggerFactory.getLogger(WatchCustomResourceDemo.class);
+
+    public static void main(String args[]) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            String namespace = "default";
+            final CountDownLatch closeLatch = new CountDownLatch(1);
+            CustomResourceDefinitionContext crdContext = new CustomResourceDefinitionContext.Builder()
+                    .withName("dummies.demo.fabric8.io")
+                    .withScope("Namespaced")
+                    .withVersion("v1")
+                    .withPlural("dummies")
+                    .withGroup("demo.fabric8.io")
+                    .build();
+
+            // Watching custom resources now
+            logger.info("Watching custom resources now");
+            client.customResource(crdContext).watch(namespace, new Watcher<String>() {
+                @Override
+                public void eventReceived(Action action, String resource) {
+                    try {
+                        JSONObject json = new JSONObject(resource);
+
+                        logger.info(action + " : " + json.getJSONObject("metadata").getString("name"));
+                    } catch (JSONException exception) {
+                        logger.error("failed to parse object");
+                    }
+                }
+
+                @Override
+                public void onClose(KubernetesClientException e) {
+                    logger.info("Watcher onClose");
+                    closeLatch.countDown();
+                    if (e != null) {
+                        logger.error(e.getMessage(), e);
+                    }
+                }
+            });
+            closeLatch.await(10, TimeUnit.MINUTES);
+        } catch (InterruptedException exception) {
+            logger.info("Interrupted: " + exception.getMessage());
+        } catch (IOException exception) {
+            logger.info("Exception: " + exception.getMessage());
+        }
+    }
+}
