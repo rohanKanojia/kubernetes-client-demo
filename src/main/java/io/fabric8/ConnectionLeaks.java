c61bbbebd324af13a10570045204e74a3edba733
Rohan Kumar <rohaan@redhat.com>
2019-11-07 23:44:09 +0530

    + Added a test case which I used for connection leaks + Updated Kubernetes client to 4.6.2

---
+++ src/main/java/io/fabric8/ConnectionLeaks.java
@@ -0,0 +1,66 @@
+package io.fabric8;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.ExecWatch;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionLeaks {
+    public static Logger logger = Logger.getLogger(ConnectionLeaks.class.getName());
+
+    public static void main(String args[]) throws InterruptedException {
+        KubernetesClient client = new DefaultKubernetesClient();
+        testConnectionLeaksAfterExecutingCommandInNonExistingPodOnRealServer(client);
+    }
+
+    public static void testConnectionLeaksAfterExecutingCommandInNonExistingPodOnRealServer(KubernetesClient client)
+            throws InterruptedException {
+        logger.info("testConnectionLeaksAfterExecutingCommandInNonExistingPodOnRealServer");
+        testConnectionLeaks(client);
+    }
+
+    private static void testConnectionLeaks(KubernetesClient client) throws InterruptedException {
+
+        int i = 1;
+        while (true) {
+            logger.info("#######################################################");
+            logger.info("####                                               ####");
+            logger.info(
+                    "####                   " + String.format("Iteration #%3d", i++) + "              ####");
+            logger.info("####                                               ####");
+            logger.info("#######################################################");
+            doIteration(client);
+            System.gc();
+        }
+    }
+
+    private static void doIteration(KubernetesClient client) throws InterruptedException {
+        logger.info("Try to execute 5 echo command in non-existing pod");
+        doExec(client);
+        doExec(client);
+        doExec(client);
+        doExec(client);
+        doExec(client);
+
+        logger.info("Wait 1 minute");
+        Thread.sleep(60 * 1000); // 1 minutes
+    }
+
+    private static void doExec(KubernetesClient client) {
+        try (ExecWatch exec =
+                     client
+                             .pods()
+                             .inNamespace("test")
+                             .withName("non-existing")
+                             .redirectingError()
+                             .exec("sh", "-c", "echo hello")) {
+            Thread.sleep(1000);
+
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "ERROR: Exception occurred: " + e.getMessage());
+        }
+    }
+
+}
