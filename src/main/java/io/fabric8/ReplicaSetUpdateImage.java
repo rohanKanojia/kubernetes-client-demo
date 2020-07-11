b295afcdeee527c356c341516c3944bfa9ef998f
Rohan Kumar <rohaan@redhat.com>
2020-07-11 12:03:37 +0530

    Examples added for new rollout API changes

--- src/main/java/io/fabric8/ReplicaSetUpdateImage.java
+++ src/main/java/io/fabric8/ReplicaSetUpdateImage.java
@@ -1,4 +1,16 @@
 package io.fabric8;
 
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
 public class ReplicaSetUpdateImage {
+    public static void main(String[] args) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            client.apps().replicaSets()
+                    .inNamespace("default")
+                    .withName("soaktestrs")
+                    .rolling()
+                    .updateImage("nickchase/soaktest");
+        }
+    }
 }
--- src/main/java/io/fabric8/StatefulSetRolloutExample.java
+++ src/main/java/io/fabric8/StatefulSetRolloutExample.java
@@ -1,4 +1,69 @@
 package io.fabric8;
 
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+import java.util.Collections;
+import java.util.Map;
+
 public class StatefulSetRolloutExample {
+    public static void main(String[] args) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            updateImage(client);
+            restart(client);
+            pause(client);
+            resume(client);
+            undo(client);
+            updateImage(client, Collections.singletonMap("nginx", "nginx:perl"));
+            System.exit(0);
+        }
+    }
+
+    private static void updateImage(KubernetesClient client) {
+        client.apps().statefulSets()
+                .inNamespace("default")
+                .withName("web")
+                .rolling()
+                .updateImage("nginx:1.19");
+    }
+
+    private static void updateImage(KubernetesClient client, Map<String, String> params) {
+        client.apps().statefulSets()
+                .inNamespace("default")
+                .withName("web")
+                .rolling()
+                .updateImage(params);
+    }
+
+    private static void restart(KubernetesClient client) {
+        client.apps().statefulSets()
+                .inNamespace("default")
+                .withName("web")
+                .rolling()
+                .restart();
+    }
+
+    private static void pause(KubernetesClient client) {
+        client.apps().statefulSets()
+                .inNamespace("default")
+                .withName("web")
+                .rolling()
+                .pause();
+    }
+
+    private static void resume(KubernetesClient client) {
+        client.apps().statefulSets()
+                .inNamespace("default")
+                .withName("web")
+                .rolling()
+                .resume();
+    }
+
+    private static void undo(KubernetesClient client) {
+        client.apps().statefulSets()
+                .inNamespace("default")
+                .withName("web")
+                .rolling()
+                .undo();
+    }
 }
