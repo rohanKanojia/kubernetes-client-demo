6e9761f40013f28b80107aeb3e02cdf5eaa5af73
Rohan Kumar <rohaan@redhat.com>
2020-10-02 18:48:10 +0530

    Bump Fabric8 Kubernetes Client to v4.12.0

--- src/main/java/io/fabric8/ExtensionsV1Beta1DeploymentCreateOrReplace.java
+++ src/main/java/io/fabric8/ExtensionsV1Beta1DeploymentCreateOrReplace.java
@@ -1,7 +1,7 @@
 package io.fabric8;
 
-import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
+import io.fabric8.kubernetes.api.model.extensions.Deployment;
+import io.fabric8.kubernetes.api.model.extensions.DeploymentBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
