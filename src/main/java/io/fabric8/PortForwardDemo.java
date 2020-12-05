5da561a9218002503acb0b4a13a1d2b5f24eb40f
rohaan@redhat.com <rohaan@redhat.com>
2020-12-05 13:15:23 +0530

    Added Port Forward Demo

---
+++ src/main/java/io/fabric8/PortForwardDemo.java
@@ -0,0 +1,32 @@
+package io.fabric8;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.LocalPortForward;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class PortForwardDemo {
+    private static final Logger logger = LoggerFactory.getLogger(PortForwardDemo.class);
+
+    public static void main(String[] args) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            String podName = "spring-boot-76dcfb669c-h4kxp";
+
+            logger.info("Starting Port Forward for pod: {} {} -> {}", podName, 80, 8080);
+            LocalPortForward portForward = client.pods()
+                    .inNamespace("default")
+                    .withName(podName)
+                    .portForward(8080, 8080);
+
+            Thread.sleep(60 * 1000);
+            logger.info("Closing...");
+            portForward.close();
+        } catch (InterruptedException | IOException interruptedException) {
+            Thread.currentThread().interrupt();
+            interruptedException.printStackTrace();
+        }
+    }
+}
