a1b8988c5eb44f051fd27e08fb6f451ec2568ebb
Rohan Kumar <rohaan@redhat.com>
2022-09-14 19:27:05 +0530

    Update PatchExamples as per latest Fabric8 Kubernetes Client
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- src/main/java/io/fabric8/DownloadFileFromMultiContainerPod.java
+++ src/main/java/io/fabric8/DownloadFileFromMultiContainerPod.java
@@ -8,16 +8,16 @@ import java.nio.file.Path;
 
 public class DownloadFileFromMultiContainerPod {
     public static void main(String[] args) {
-try (KubernetesClient client = new KubernetesClientBuilder().build()) {
-    // Path Where to copy file to local storage
-    Path downloadToPath = new File("/home/rohaan/Downloads/docker-entrypoint.sh").toPath();
-    // Using Kubernetes Client to copy file from pod
-    client.pods()
-            .inNamespace("default")             // <- Namespace of pod
-            .withName("multi-container-pod")    // <- Name of pod
-            .inContainer("c1")                  // <- Container from which file has to be downloaded
-            .file("/docker-entrypoint.sh")      // <- Path of file inside pod
-            .copy(downloadToPath);                // <- Local path where to copy downloaded file
-}
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+            // Path Where to copy file to local storage
+            Path downloadToPath = new File("/home/rohaan/Downloads/docker-entrypoint.sh").toPath();
+            // Using Kubernetes Client to copy file from pod
+            client.pods()
+                .inNamespace("default")             // <- Namespace of pod
+                .withName("multi-container-pod")    // <- Name of pod
+                .inContainer("c1")                  // <- Container from which file has to be downloaded
+                .file("/docker-entrypoint.sh")      // <- Path of file inside pod
+                .copy(downloadToPath);                // <- Local path where to copy downloaded file
+        }
     }
 }
--- src/main/java/io/fabric8/PatchExamples.java
+++ src/main/java/io/fabric8/PatchExamples.java
@@ -14,105 +14,97 @@ import java.util.Collections;
 import java.util.List;
 
 public class PatchExamples {
-    public static void main(String[] args) {
-        try (KubernetesClient kubernetesClient = new KubernetesClientBuilder().build()) {
-            Deployment deployment = createNewDeployment("patch-demo", 2);
+  public static void main(String[] args) {
+    try (KubernetesClient kubernetesClient = new KubernetesClientBuilder().build()) {
+      Deployment deployment = createNewDeployment("patch-demo", 2);
 
-            deployment = kubernetesClient.apps().deployments()
-                    .inNamespace("default")
-                .resource(deployment)
-                    .createOrReplace();
+      deployment = kubernetesClient.apps().deployments()
+          .inNamespace("default")
+          .resource(deployment)
+          .createOrReplace();
 
-            standardPatchTyped(kubernetesClient, deployment);
-            patchStrategicMerge(kubernetesClient, deployment);
-            patchStrategicMergeExplicit(kubernetesClient, deployment);
-            patchJson(kubernetesClient, deployment);
-            patchJsonMerge(kubernetesClient, deployment);
-        } catch (KubernetesClientException e) {
-            System.out.println("Exception received: " + e.getMessage());
-        }
+      standardPatchTyped(kubernetesClient, deployment);
+      patchStrategicMerge(kubernetesClient, deployment);
+      patchStrategicMergeExplicit(kubernetesClient, deployment);
+      patchJson(kubernetesClient, deployment);
+      patchJsonMerge(kubernetesClient, deployment);
+    } catch (KubernetesClientException e) {
+      System.out.println("Exception received: " + e.getMessage());
     }
+  }
 
-    private static void patchStrategicMergeExplicit(KubernetesClient kubernetesClient, Deployment deployment) {
-        PatchContext patchContext = new PatchContext.Builder()
-                .withPatchType(PatchType.STRATEGIC_MERGE)
-                .build();
+  private static void patchStrategicMergeExplicit(KubernetesClient kubernetesClient, Deployment deployment) {
+    kubernetesClient.apps().deployments()
+        .inNamespace(deployment.getMetadata().getNamespace())
+        .withName(deployment.getMetadata().getName())
+        .patch(
+            PatchContext.of(PatchType.STRATEGIC_MERGE),
+            "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"patch-demo-ctr-2\",\"image\":\"redis\"}]}}}}");
+  }
 
-        kubernetesClient.apps().deployments()
-                .inNamespace(deployment.getMetadata().getNamespace())
-                .withName(deployment.getMetadata().getName())
-                .patch(patchContext, "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"patch-demo-ctr-2\",\"image\":\"redis\"}]}}}}");
-    }
+  private static void standardPatchTyped(KubernetesClient kubernetesClient, Deployment deployment) {
+    // Prepare modified patch object
+    List<Container> containerList = deployment.getSpec().getTemplate().getSpec().getContainers();
+    containerList.add(new ContainerBuilder()
+        .withName("patch-demo-ctr-2")
+        .withImage("redis")
+        .build());
+    deployment.getSpec().getTemplate().getSpec().setContainers(containerList);
 
-    private static void standardPatchTyped(KubernetesClient kubernetesClient, Deployment deployment) {
-        // Prepare modified patch object
-        List<Container> containerList = deployment.getSpec().getTemplate().getSpec().getContainers();
-        containerList.add(new ContainerBuilder()
-                .withName("patch-demo-ctr-2")
-                .withImage("redis")
-                .build());
-        deployment.getSpec().getTemplate().getSpec().setContainers(containerList);
+    // Patch to API Server
+    kubernetesClient.apps().deployments()
+        .inNamespace(deployment.getMetadata().getNamespace())
+        .resource(deployment)
+        .patch();
+  }
 
-        // Patch to API Server
-        kubernetesClient.apps().deployments()
-                .inNamespace(deployment.getMetadata().getNamespace())
-                .resource(deployment)
-                .patch();
-    }
+  private static void patchJsonMerge(KubernetesClient kubernetesClient, Deployment deployment) {
+    kubernetesClient.apps().deployments()
+        .inNamespace(deployment.getMetadata().getNamespace())
+        .withName(deployment.getMetadata().getName())
+        .patch(PatchContext.of(PatchType.JSON_MERGE), "{\"metadata\":{\"annotations\":{\"foo\":null}}}");
+  }
 
-    private static void patchJsonMerge(KubernetesClient kubernetesClient, Deployment deployment) {
-        PatchContext patchContext = new PatchContext.Builder()
-                .withPatchType(PatchType.JSON_MERGE)
-                .build();
+  private static void patchJson(KubernetesClient kubernetesClient, Deployment deployment) {
+    kubernetesClient.apps().deployments()
+        .inNamespace(deployment.getMetadata().getNamespace())
+        .withName(deployment.getMetadata().getName())
+        .patch(
+            PatchContext.of(PatchType.JSON),
+            "[{\"op\": \"test\", \"path\":\"/metadata/labels\", \"value\":null}]");
+  }
 
-        kubernetesClient.apps().deployments()
-                .inNamespace(deployment.getMetadata().getNamespace())
-                .withName(deployment.getMetadata().getName())
-                .patch(patchContext, "{\"metadata\":{\"annotations\":{\"foo\":null}}}");
-    }
+  private static void patchStrategicMerge(KubernetesClient kubernetesClient, Deployment deployment) {
+    kubernetesClient.apps().deployments()
+        .inNamespace(deployment.getMetadata().getNamespace())
+        .withName(deployment.getMetadata().getName())
+        .patch("{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"patch-demo-ctr-2\",\"image\":\"redis\"}]}}}}");
+  }
 
-    private static void patchJson(KubernetesClient kubernetesClient, Deployment deployment) {
-        PatchContext patchContext = new PatchContext.Builder()
-                .withPatchType(PatchType.JSON)
-                .build();
-
-        kubernetesClient.apps().deployments()
-                .inNamespace(deployment.getMetadata().getNamespace())
-                .withName(deployment.getMetadata().getName())
-                .patch(patchContext, "[{\"op\": \"test\", \"path\":\"/metadata/labels\", \"value\":null}]");
-    }
-
-    private static void patchStrategicMerge(KubernetesClient kubernetesClient, Deployment deployment) {
-        kubernetesClient.apps().deployments()
-                .inNamespace(deployment.getMetadata().getNamespace())
-                .withName(deployment.getMetadata().getName())
-                .patch("{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"patch-demo-ctr-2\",\"image\":\"redis\"}]}}}}");
-    }
-
-    private static Deployment createNewDeployment(String name, int replicas) {
-        return new DeploymentBuilder()
-                .withNewMetadata()
-                .withName(name)
-                .addToAnnotations("foo", "bar")
-                .addToAnnotations("app", "nginx")
-                .endMetadata()
-                .withNewSpec()
-                .withReplicas(replicas)
-                .withNewSelector()
-                .addToMatchLabels(Collections.singletonMap("app", "nginx"))
-                .endSelector()
-                .withNewTemplate()
-                .withNewMetadata()
-                .addToLabels("app", "nginx")
-                .endMetadata()
-                .withNewSpec()
-                .addNewContainer()
-                .withName("patch-demo-ctr")
-                .withImage("nginx")
-                .endContainer()
-                .endSpec()
-                .endTemplate()
-                .endSpec()
-                .build();
-    }
+  private static Deployment createNewDeployment(String name, int replicas) {
+    return new DeploymentBuilder()
+        .withNewMetadata()
+        .withName(name)
+        .addToAnnotations("foo", "bar")
+        .addToAnnotations("app", "nginx")
+        .endMetadata()
+        .withNewSpec()
+        .withReplicas(replicas)
+        .withNewSelector()
+        .addToMatchLabels(Collections.singletonMap("app", "nginx"))
+        .endSelector()
+        .withNewTemplate()
+        .withNewMetadata()
+        .addToLabels("app", "nginx")
+        .endMetadata()
+        .withNewSpec()
+        .addNewContainer()
+        .withName("patch-demo-ctr")
+        .withImage("nginx")
+        .endContainer()
+        .endSpec()
+        .endTemplate()
+        .endSpec()
+        .build();
+  }
 }
