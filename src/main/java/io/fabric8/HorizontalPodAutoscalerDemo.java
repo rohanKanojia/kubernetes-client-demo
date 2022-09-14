a72390f818edb2f1f0ec6951f8893853dd9b6629
Rohan Kumar <rohaan@redhat.com>
2022-09-14 19:51:12 +0530

    Add example for HorizontalPodAutoscaler
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ src/main/java/io/fabric8/HorizontalPodAutoscalerDemo.java
@@ -0,0 +1,43 @@
+package io.fabric8;
+
+import io.fabric8.kubernetes.api.model.Quantity;
+import io.fabric8.kubernetes.api.model.autoscaling.v1.HorizontalPodAutoscaler;
+import io.fabric8.kubernetes.api.model.autoscaling.v1.HorizontalPodAutoscalerBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+
+public class HorizontalPodAutoscalerDemo {
+  public static void main(String[] args) {
+    try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+      HorizontalPodAutoscaler hpa = createHPA();
+      client.autoscaling().v1().horizontalPodAutoscalers()
+          .resource(hpa)
+          .createOrReplace();
+      System.out.println(hpa.getMetadata().getName() + " created!");
+
+      client.autoscaling().v1().horizontalPodAutoscalers()
+          .resource(hpa)
+          .delete();
+      System.out.println(hpa.getMetadata().getName() + " deleted!");
+    }
+
+  }
+
+  private static HorizontalPodAutoscaler createHPA() {
+    return new HorizontalPodAutoscalerBuilder()
+        .withNewMetadata()
+        .withName("foo-hpa")
+        .addToLabels("name", "foo")
+        .endMetadata()
+        .withNewSpec()
+        .withNewScaleTargetRef()
+        .withApiVersion("apps/v1")
+        .withKind("Deployment")
+        .withName("foo")
+        .endScaleTargetRef()
+        .withMinReplicas(1)
+        .withMaxReplicas(10)
+        .endSpec()
+        .build();
+  }
+}
