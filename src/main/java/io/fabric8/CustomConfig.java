bc6cbe913f87ff1c7a66529980248590ede63229
Rohan Kumar <rohaan@redhat.com>
2024-08-13 10:01:39 +0530

    chore (deps) : Update Fabric8 Kubernetes Client to v6.13.2
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- src/main/java/io/fabric8/CustomConfig.java
+++ src/main/java/io/fabric8/CustomConfig.java
@@ -7,8 +7,10 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 
 public class CustomConfig {
     public static void main(String[] args) {
+        int max = 512;
         System.setProperty("kubernetes.disable.autoConfig", "true");
-        Config config = new ConfigBuilder()
+        Config config = new ConfigBuilder(Config.autoConfigure(null))
+          .withMaxConcurrentRequests(max    )
                 .build();
         try (KubernetesClient client = new KubernetesClientBuilder().withConfig(config).build()) {
             client.pods().list();
